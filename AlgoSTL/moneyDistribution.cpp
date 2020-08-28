73d2d4c5fb699347f5ed12f8b05784acd972d14a
Rakesh <rakeshs@cdot.in>
2020-08-28 12:02:52 +0530

    changes in AlgoSTL dir

--- AlgoSTL/moneyDistribution.cpp
+++ AlgoSTL/moneyDistribution.cpp
@@ -12,10 +12,10 @@ int main()
 {
 	int a[]={1,2,5,10,20,50,100,500,2000};
 	int n=sizeof(a)/sizeof(int);
-	int key=100;
+	int key=37;
 	while(key>0)
 	{
-		auto lb = lower_bound(a,a+n,key);
+		auto lb = upper_bound(a,a+n,key);
 		int index = lb-a-1;
 		int coin = a[index];
 		cout << coin <<" , ";
