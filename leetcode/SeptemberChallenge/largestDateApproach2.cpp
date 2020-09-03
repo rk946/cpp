bd465a56e7974803a3e3b9a843c4346ef6a5e521
Rakesh <rakeshs@cdot.in>
2020-09-03 16:21:32 +0530

    september challenge problem 3

--- leetcode/SeptemberChallenge/largestDateApproach2.cpp
+++ leetcode/SeptemberChallenge/largestDateApproach2.cpp
@@ -4,44 +4,7 @@ using namespace std;
 
  string largestTimeFromDigits(vector<int>& A) {
         string str;
-        sort(A.begin(),A.end());
-        int a;
-     	for(int i=0;i<5;i++)
-     	{
-     		if(i==0)
-     			a=2;
-     		else if(i==1)
-     		{
-     			if(str[0] == '2')
-     				a=3;
-     			else
-     				a=10;
-     		}
-     		else if(i==2)
-     		{
-     			str+=":";
-     			continue;
-     		}
-     		else if(i==3)
-     			a=5;
-     		else
-     			a=10;
-     		
-     		int j=3;
-     		for(;j>=0;j--)
-     		{
-     			if(A[j]!=-1 and A[j]<=a)
-     			{
-     				str+=A[j]+48;
-     				A[j]=-1;
-     				break;
-     			}
-     		}
-     		if(j==-1)
-     			return "";
-     	}
-     	
-        return str;
+        
     }
 
 
